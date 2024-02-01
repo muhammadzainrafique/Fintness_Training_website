b4be5a67cf05a238f498f5a172464b1da98de988
muhammadzainrafique <147074765+muhammadzainrafique@users.noreply.github.com>
2024-02-01 14:08:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById('button');
+    button.addEventListener('click',()=>{
+        let weight = document.querySelector('#weight').value;
+        let height = document.querySelector('#height').value;
+        let result = document.querySelector('#result');
+        result.value =(+weight/((+height)**2)).toFixed(2)
+    })
+
+    const observer = new IntersectionObserver((entries)=>{
+        entries.forEach((entry)=>{
+            console.log(entry);
+            if(entry.isIntersecting){
+                entry.target.classList.add('show');
+            }
+            else{
+                entry.target.classList.remove('show');
+            }
+        })
+    })
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el)=> observer.observe(el));
+   
+});
